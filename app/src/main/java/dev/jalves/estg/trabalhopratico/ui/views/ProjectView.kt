d7198cd7771bb36ec9afc76c8601857ca81a4be5
João Alves <joao.2003.couto@gmail.com>
2025-05-21 19:38:37 +0100

    feat: added dropdown menu to project view

--- app/src/main/java/dev/jalves/estg/trabalhopratico/ui/views/ProjectView.kt
+++ app/src/main/java/dev/jalves/estg/trabalhopratico/ui/views/ProjectView.kt
@@ -1,18 +1,60 @@
 package dev.jalves.estg.trabalhopratico.ui.views
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Folder
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.TableChart
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+
+@Composable
+fun MenuItem(
+    icon: ImageVector,
+    text: String
+) {
+    CompositionLocalProvider(
+        LocalContentColor provides MaterialTheme.colorScheme.onSurfaceVariant,
+        LocalTextStyle provides LocalTextStyle.current.copy(
+            fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+        )
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(icon, contentDescription = text)
+            Text(text)
+        }
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -20,6 +62,8 @@ fun ProjectView(
     navController: NavHostController,
     projectID: Int
 ) {
+    var expanded by remember { mutableStateOf(false) }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -31,6 +75,58 @@ fun ProjectView(
                         Icon(
                             imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                             contentDescription = "Back"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { expanded = true }) {
+                        Icon(Icons.Default.MoreVert, contentDescription = "Menu")
+                    }
+
+                    DropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false }
+                    ) {
+                        DropdownMenuItem(
+                            text = { MenuItem(Icons.Default.Edit, "Edit manager") },
+                            onClick = {
+                                expanded = false
+                            }
+                        )
+
+                        DropdownMenuItem(
+                            text = { MenuItem(Icons.Default.Edit, "Edit") },
+                            onClick = {
+                                expanded = false
+                            }
+                        )
+
+                        DropdownMenuItem(
+                            text = { MenuItem(Icons.Default.Delete, "Delete") },
+                            onClick = {
+                                expanded = false
+                            }
+                        )
+
+                        DropdownMenuItem(
+                            text = { MenuItem(Icons.Default.TableChart, "Export stats") },
+                            onClick = {
+                                expanded = false
+                            }
+                        )
+
+                        DropdownMenuItem(
+                            text = { MenuItem(Icons.Default.Check, "Mark complete") },
+                            onClick = {
+                                expanded = false
+                            }
+                        )
+
+                        DropdownMenuItem(
+                            text = { MenuItem(Icons.Default.Folder, "Archive project") },
+                            onClick = {
+                                expanded = false
+                            }
                         )
                     }
                 }
