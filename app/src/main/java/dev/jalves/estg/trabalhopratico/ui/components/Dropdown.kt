9fa93e0b76f377b58aa725dc4072add5ca58daa2
Pedro Cunha <pedro10cunha@gmail.com>
2025-06-02 10:19:35 +0100

    feat: Better Dropdown container style

--- app/src/main/java/dev/jalves/estg/trabalhopratico/ui/components/Dropdown.kt
+++ app/src/main/java/dev/jalves/estg/trabalhopratico/ui/components/Dropdown.kt
@@ -1,9 +1,12 @@
 package dev.jalves.estg.trabalhopratico.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.KeyboardArrowDown
 import androidx.compose.material3.DropdownMenu
@@ -14,6 +17,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -26,6 +31,9 @@ fun Dropdown(
 ) {
     Box {
         Row(
+            modifier = Modifier.clip(RoundedCornerShape(5.dp))
+                .background(MaterialTheme.colorScheme.surfaceContainer)
+                .padding(10.dp),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.spacedBy(4.dp)
         ) {
