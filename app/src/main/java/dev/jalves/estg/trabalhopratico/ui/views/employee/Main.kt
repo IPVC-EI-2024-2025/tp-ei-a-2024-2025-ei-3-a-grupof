ed31375a4ed7be744cbc36c2a61ef8e2ce8b5f5f
Pedro Cunha <pedro10cunha@gmail.com>
2025-06-09 10:58:04 +0100

    feat: Added packages for each role

---
+++ app/src/main/java/dev/jalves/estg/trabalhopratico/ui/views/employee/Main.kt
@@ -0,0 +1,70 @@
+package dev.jalves.estg.trabalhopratico.ui.views.employee
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import dev.jalves.estg.trabalhopratico.R
+import dev.jalves.estg.trabalhopratico.ui.views.MenuView
+import dev.jalves.estg.trabalhopratico.ui.views.ProfileViewModel
+import dev.jalves.estg.trabalhopratico.ui.views.ProjectListView
+import dev.jalves.estg.trabalhopratico.ui.views.ProjectsViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EmployeeMain(
+    rootNavController: NavHostController,
+    profileViewModel: ProfileViewModel,
+    projectsViewModel: ProjectsViewModel
+) {
+    val navController = rememberNavController()
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = { Image(
+                    painter = painterResource(id = R.drawable.logo_h),
+                    contentDescription = "TaskSync logo",
+                    modifier = Modifier.size(128.dp)
+                ) }
+            )
+        },
+        bottomBar = {
+            EmployeeNavBar(navController)
+        }
+    ) { innerPadding ->
+        NavHost(
+            modifier = Modifier.padding(innerPadding),
+            navController = navController,
+            startDestination = "home"
+        ) {
+            composable(route = "home") {
+                EmployeeHome(rootNavController, profileViewModel)
+            }
+
+            composable(route = "tasks") {
+                ProjectListView(rootNavController, projectsViewModel)
+            }
+
+            composable(route = "projects") {
+                ProjectListView(rootNavController, projectsViewModel)
+            }
+
+            composable(route = "menu") {
+                MenuView(rootNavController)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/dev/jalves/estg/trabalhopratico/ui/views/manager/Main.kt
@@ -0,0 +1,71 @@
+package dev.jalves.estg.trabalhopratico.ui.views.manager
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import dev.jalves.estg.trabalhopratico.R
+import dev.jalves.estg.trabalhopratico.ui.views.MenuView
+import dev.jalves.estg.trabalhopratico.ui.views.ProfileViewModel
+import dev.jalves.estg.trabalhopratico.ui.views.ProjectListView
+import dev.jalves.estg.trabalhopratico.ui.views.ProjectsViewModel
+import dev.jalves.estg.trabalhopratico.ui.views.UserListView
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ManagerMain(
+    rootNavController: NavHostController,
+    profileViewModel: ProfileViewModel,
+    projectsViewModel: ProjectsViewModel
+) {
+    val navController = rememberNavController()
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = { Image(
+                    painter = painterResource(id = R.drawable.logo_h),
+                    contentDescription = "TaskSync logo",
+                    modifier = Modifier.size(128.dp)
+                ) }
+            )
+        },
+        bottomBar = {
+            ManagerNavBar(navController)
+        }
+    ) { innerPadding ->
+        NavHost(
+            modifier = Modifier.padding(innerPadding),
+            navController = navController,
+            startDestination = "home"
+        ) {
+            composable(route = "home") {
+                ManagerHome(rootNavController, profileViewModel)
+            }
+
+            composable(route = "projects") {
+                ProjectListView(rootNavController, projectsViewModel)
+            }
+
+            composable(route = "users") {
+                UserListView()
+            }
+
+            composable(route = "menu") {
+                MenuView(rootNavController)
+            }
+        }
+    }
+}
