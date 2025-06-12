981b4e7cba2a78d5fe1f2d5575222c7bf52f7557
Pedro Cunha <pedro10cunha@gmail.com>
2025-06-12 02:31:45 +0100

    fix: apply material theme to sign-in page with scaffold

--- app/src/main/java/dev/jalves/estg/trabalhopratico/ui/views/SignInView.kt
+++ app/src/main/java/dev/jalves/estg/trabalhopratico/ui/views/SignInView.kt
@@ -13,9 +13,12 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -33,6 +36,7 @@ import dev.jalves.estg.trabalhopratico.R
 import dev.jalves.estg.trabalhopratico.services.AuthService
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SignIn(
     onRegisterButtonClicked: () -> Unit,
@@ -46,83 +50,94 @@ fun SignIn(
 
     var isLoading by remember { mutableStateOf(false) }
 
-    Column(
+    Scaffold(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.logo),
-            contentDescription = "TaskSync logo",
-            modifier = Modifier.size(192.dp)
-        )
-
-        OutlinedTextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text("Email") }
-        )
-
-        OutlinedTextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Password") },
-            visualTransformation = PasswordVisualTransformation(),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-        )
-
+        topBar = {
+            TopAppBar(
+                title = { }
+            )
+        }
+    ) { innerPadding ->
         Column(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Button(
-                modifier = Modifier.height(64.dp).padding(
-                    horizontal = 16.dp,
-                    vertical = 8.dp
-                ),
-                onClick = {
-                    if(isLoading || email.isEmpty() || password.isEmpty())
-                        return@Button
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "TaskSync logo",
+                modifier = Modifier.size(192.dp)
+            )
 
-                    isLoading = true
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") }
+            )
 
-                    scope.launch {
-                        try {
-                            val result = AuthService.signIn(context, email, password)
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            )
 
-                            if(result.isSuccess) {
-                                onSuccessfulSignIn()
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(
+                    modifier = Modifier.height(64.dp).padding(
+                        horizontal = 16.dp,
+                        vertical = 8.dp
+                    ),
+                    onClick = {
+                        if(isLoading || email.isEmpty() || password.isEmpty())
+                            return@Button
+
+                        isLoading = true
+
+                        scope.launch {
+                            try {
+                                val result = AuthService.signIn(context, email, password)
+
+                                if(result.isSuccess) {
+                                    onSuccessfulSignIn()
+                                }
+                            } catch (e: Exception) {
+                                Log.e("SignUp", "Failed to sign in", e)
+                                Toast.makeText(
+                                    context,
+                                    e.message,
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            } finally {
+                                isLoading = false
                             }
-                        } catch (e: Exception) {
-                            Log.e("SignUp", "Failed to sign in", e)
-                            Toast.makeText(
-                                context,
-                                e.message,
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        } finally {
-                            isLoading = false
                         }
+                    },
+                ) {
+                    if (isLoading) {
+                        CircularProgressIndicator(
+                            modifier = Modifier.size(24.dp),
+                            strokeWidth = 2.dp
+                        )
+                    } else {
+                        Text("Login")
                     }
-                },
-            ) {
-                if (isLoading) {
-                    CircularProgressIndicator(
-                        modifier = Modifier.size(24.dp),
-                        strokeWidth = 2.dp
-                    )
-                } else {
-                    Text("Login")
                 }
-            }
 
-            TextButton(
-                modifier = Modifier.height(36.dp),
-                onClick = {
-                    onRegisterButtonClicked()
+                TextButton(
+                    modifier = Modifier.height(36.dp),
+                    onClick = {
+                        onRegisterButtonClicked()
+                    }
+                ) {
+                    Text("Sign up")
                 }
-            ) {
-                Text("Sign up")
             }
         }
     }
